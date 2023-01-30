e57cb4ec68721417c2e26cf3ab9838b3a2ef4e70
heejong-kim <heejong.hj.kim@gmail.com>
2023-01-30 13:16:47 -0500

    init

--- train-PaIRNet-longitudinal.py
+++ train-PaIRNet-longitudinal.py
@@ -353,7 +353,7 @@ parser.add_argument('--max_iters', default=10000000000, type=int, help="Max iter
 parser.add_argument('--epoch', default=0, type=int, help="Starting epoch")
 parser.add_argument('--num_workers', default=12, type=int)
 
-parser.add_argument('--imagesize', default=[68, 68], type=list, help="image size [x, y]")
+parser.add_argument('--imagesize', default="[68, 68]", type=list, help="image size [x, y]")
 parser.add_argument('--image_channel', default=1, type=int)
 parser.add_argument('--imagedir', default='./datasets/starmen-augmentation', type=str)
 parser.add_argument('--targetname', default='timepoint', type=str)
@@ -367,6 +367,7 @@ suffix = f'seed{torch.initial_seed()}'
 dict_dataloader = {'starmen': STARMEN, 'tumor': TUMOR,
                    'embryo': EMBRYO, 'oasis': OASIS}
 
+print(opt)
 if __name__ == "__main__":
 
     # train PaIRNet
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import numpy as np
+import random
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
