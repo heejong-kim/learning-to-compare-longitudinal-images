678af5c26eca503cf54738627d14e02426bb3754
heejong-kim <heejong.hj.kim@gmail.com>
2023-01-30 23:12:45 -0500

    init

--- example-script-tumor.sh
+++ example-script-tumor.sh
@@ -13,12 +13,12 @@ python ./train-PaIRNet-longitudinal.py --max_epoch=1 --num_workers=1 --image_siz
 python ./train-baseline-crosssectional-regression.py --max_epoch=1 --num_workers=1 --image_size="200,200" --image_channel=3 --targetname='size'\
 --image_dir='./data/tumor' --dataname='tumor'
 
-## testing: weighted CAM visualization
-python ./test-weightedCAM.py --image_size="200,200" --image_channel=3 --image_dir='./data/tumor' --dataname='tumor'\
---save_name=
-
-## testing: correlation between predicted delta and groundtruth
-python ./test-correlation.py --image_size="200,200" --image_channel=3 --image_dir='./data/tumor' --dataname='tumor'\
---save_name=
+### testing: weighted CAM visualization
+#python ./test-weightedCAM.py --image_size="200,200" --image_channel=3 --image_dir='./data/tumor' --dataname='tumor'\
+#--save_name=
+#
+### testing: correlation between predicted delta and groundtruth
+#python ./test-correlation.py --image_size="200,200" --image_channel=3 --image_dir='./data/tumor' --dataname='tumor'\
+#--save_name=
 
 
