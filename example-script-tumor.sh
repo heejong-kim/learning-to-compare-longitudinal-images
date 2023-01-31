fe11570b21be61bbfd07fb812f2976fbf48f8dcc
heejong-kim <heejong.hj.kim@gmail.com>
2023-01-31 10:32:09 -0500

    init

--- example-script-tumor.sh
+++ example-script-tumor.sh
@@ -2,20 +2,18 @@
 
 
 ## training: self-supervised PaIRNet
-python ./train-PaIRNet-longitudinal.py --max_epoch=5 --num_workers=1 --image_size="200,200" --image_channel=3 --targetname='timepoint' --image_dir='./data/tumor' --dataname='tumor' --selfsupervised
+python ./train-PaIRNet-longitudinal.py --max_epoch=3 --num_workers=1 --image_size="200,200" --image_channel=3 --targetname='timepoint' --image_dir='./data/tumor' --dataname='tumor' --selfsupervised --num_of_iters=20
 
 ## training: supervised PaIRNet
-python ./train-PaIRNet-longitudinal.py --max_epoch=1 --num_workers=1 --image_size="200,200" --image_channel=3 --targetname='timepoint' --image_dir='./data/tumor' --dataname='tumor' --no-selfsupervised
+python ./train-PaIRNet-longitudinal.py --max_epoch=3 --num_workers=1 --image_size="200,200" --image_channel=3 --targetname='timepoint' --image_dir='./data/tumor' --dataname='tumor' --no-selfsupervised --num_of_iters=20
 
 ## training: baseline crosssectional regression
-python ./train-baseline-crosssectional-regression.py --max_epoch=1 --num_workers=1 --image_size="200,200" --image_channel=3 --targetname='size' --image_dir='./data/tumor' --dataname='tumor'
+python ./train-baseline-crosssectional-regression.py --max_epoch=3 --num_workers=1 --image_size="200,200" --image_channel=3 --targetname='size' --image_dir='./data/tumor' --dataname='tumor' --num_of_iters=20
 
 ### testing: weighted CAM visualization
-#python ./test-weightedCAM.py --image_size="200,200" --image_channel=3 --image_dir='./data/tumor' --dataname='tumor'\
-#--save_name=
-#
-### testing: correlation between predicted delta and groundtruth
-#python ./test-correlation.py --image_size="200,200" --image_channel=3 --image_dir='./data/tumor' --dataname='tumor'\
-#--save_name=
+python ./test-weightedCAM.py --image_size="200,200" --image_channel=3 --image_dir='./data/tumor' --dataname='tumor' --save_name='./result-model/tumor/lr0.01-b10.9-b20.999seed0/PaIRNet-self-supervised/best.pth'
+
+## testing: correlation between predicted delta and groundtruth
+python ./test-correlation.py --image_size="200,200" --image_channel=3 --image_dir='./data/tumor' --dataname='tumor' --save_name='./result-model/tumor/lr0.01-b10.9-b20.999seed0/PaIRNet-self-supervised/best.pth'
 
 
